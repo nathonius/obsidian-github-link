144013cee93f81e307d7160e7ac3b86d9aed34a9
Nathan Smith <nathan@nathan-smith.org>
2024-04-06 13:31:27 -0400

    🔨 refactor: #84 Move account settings into new class

---
+++ src/settings/index.ts
@@ -0,0 +1,3 @@
+export type { GithubAccount, GithubLinkPluginData, GithubLinkPluginSettings } from "./types";
+export { DEFAULT_SETTINGS } from "./types";
+export { GithubLinkPluginSettingsTab } from "./settings-tab";
